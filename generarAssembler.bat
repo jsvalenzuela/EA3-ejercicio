0f529017610e692222d8165e6211e2cc579469ce
Santiago Valenzuela <asanti1195@gmail.com>
2020-07-11 22:22:29 -0300

    Se corrige el archivo de testing

--- generarAssembler.bat
+++ generarAssembler.bat
@@ -5,13 +5,11 @@ C:\GnuWin32\bin\flex Lexico.l
 pause
 C:\MinGW\bin\gcc.exe  -std=c99 terceto.c prints.c archivos.c ts.c y.tab.c lex.yy.c assembler.c -o .\build\final.exe
 pause
-type prueba.txt | .\build\final.exe
+type testing.txt | .\build\final.exe
 
 
 pause
 :: del ts.txt
 ::del intermedia.txt
 ::del status.txt
-::echo "PRUEBA - OK TERCETOS ASIG MULT"
-::type .\tests\prueba.txt | .\build\Grupo10.exe
 ::pause
