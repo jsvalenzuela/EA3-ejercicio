353851b9bae2b6afb4710bf589c9ca2bf2197271
Santiago Valenzuela <asanti1195@gmail.com>
2020-07-14 11:26:28 -0300

    Se corrige la funcion de tercetos

--- terceto.c
+++ terceto.c
@@ -41,7 +41,9 @@ void crearTercetos(ArrayTercetos * a, size_t n)
 
 void insertarTercetos(ArrayTercetos *a, Terceto element) 
 {
-    if (a->tamanioUsado == a->tamanioTotal)
+  
+    // Copiar Terceto
+if (a->tamanioUsado == a->tamanioTotal)
     {
         a->tamanioTotal *= 2;
         a->array = (Terceto *)realloc(a->array, a->tamanioTotal * sizeof(Terceto));
@@ -49,49 +51,13 @@ void insertarTercetos(ArrayTercetos *a, Terceto element)
 
     // Copiar Terceto
 
-    a->array[a->tamanioUsado].tercetoID = element.tercetoID;
-	
-    if(element.isOperator == 1) {
-		if(element.operator == TOP_PRINT || element.operator == TOP_READ)
-		{
-			a->array[a->tamanioUsado].isOperand = 0;
-			a->array[a->tamanioUsado].isOperator = 1;
-			a->array[a->tamanioUsado].operator = element.operator;
-			a->array[a->tamanioUsado].type = element.type;
-			a->array[a->tamanioUsado].stringValue = malloc(strlen(element.stringValue) + 1);
-            strcpy(a->array[a->tamanioUsado].stringValue, element.stringValue);
-		}else{
-		
-			a->array[a->tamanioUsado].isOperand = 0;
-			a->array[a->tamanioUsado].isOperator = 1;
-			a->array[a->tamanioUsado].operator = element.operator;
-			a->array[a->tamanioUsado].left = element.left;
-			a->array[a->tamanioUsado].right = element.right;
-
-			if(element.operator == TOP_JUMP) {
-				a->array[a->tamanioUsado].operatorStringValue = malloc(strlen(element.operatorStringValue) + 1);
-				strcpy(a->array[a->tamanioUsado].operatorStringValue, element.operatorStringValue);
-			}
-		}
-    }
-
-    if(element.isOperand == 1) {
-        a->array[a->tamanioUsado].isOperator = 0;
-        a->array[a->tamanioUsado].isOperand = 1;
-        a->array[a->tamanioUsado].type = element.type;
-        
-        if(element.type == 'S') {
-            // Puede ser variable o Const string -> puede convenir un type V
-            a->array[a->tamanioUsado].stringValue = malloc(strlen(element.stringValue) + 1);
-            strcpy(a->array[a->tamanioUsado].stringValue, element.stringValue);
-        }
-        else if (element.type == 'F') {
-            a->array[a->tamanioUsado].floatValue = element.floatValue;
-        } 
-        else if (element.type == 'I') {
-            a->array[a->tamanioUsado].intValue = element.intValue;
-        }
-    }
+		a->array[a->tamanioUsado].tercetoID = element.tercetoID;
+		a->array[a->tamanioUsado].isOperand = 0;
+		a->array[a->tamanioUsado].isOperator = 1;
+		a->array[a->tamanioUsado].operator = element.operator;
+		a->array[a->tamanioUsado].type = element.type;
+		a->array[a->tamanioUsado].stringValue = malloc(strlen(element.stringValue) + 1);
+        strcpy(a->array[a->tamanioUsado].stringValue, element.stringValue);
 
     a->tamanioUsado = a->tamanioUsado + 1;
 };
